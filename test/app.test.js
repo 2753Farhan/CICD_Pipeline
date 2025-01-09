9d27f5eb68c73ef3f459540940fb95d2c80ef6ec
Md Farhan Islam Shuvro <shuvro.cadet@gmail.com>
2025-01-09 11:03:03 +0600

    modified

--- test/app.test.js
+++ test/app.test.js
@@ -2,9 +2,9 @@ const request = require("supertest");
 const app = require("../");
 
 describe("GET /", () => {
-  it("should return 'Hello, World this is a practice for CICD!'", async () => {
+  it("should return 'Hello, World this is a practice for CICD.'", async () => {
     const response = await request(app).get("/");
     expect(response.statusCode).toBe(200);
-    expect(response.text).toBe("Hello, World this is a practice for CICD!");
+    expect(response.text).toBe("Hello, World this is a practice for CICD.");
   });
 });
